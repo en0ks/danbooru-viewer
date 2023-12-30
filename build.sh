b0c2044ce3fa0f727fb8e30b2d7bccee041e4807
en0ks <enesskaya61@gmail.com>
2023-12-31 00:24:37 +0100

    * update structure *

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-if ! [ -d ./lib/org/ ]; then
-    jar xvf ./lib/jsoup-1.17.2.jar
+if ! [ -d ./classes/org/jsoup/ ]; then
+    jar xvf ./lib/jsoup-1.17.2.jar && rm -rf ./lib/META_INF/
     mv ./lib/org/ ./classes/
 fi
 javac -d ./classes/ -cp "./lib/jsoup-1.17.2.jar" ./src/*.java
