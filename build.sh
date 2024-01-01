54a3b73e15ed16c9ba44dcd756a16a2bebde6a72
en0ks <enesskaya61@gmail.com>
2024-01-01 12:54:21 +0100

    * changed build *

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ if ! [ -d ./classes/org/json/ ]; then
     jar xvf ./lib/org.json-chargebee-1.0.jar && mv ./lib/org/json ./classes/org/
 fi
 find ./lib/ -type f ! -name '*.jar' -delete
+rm -rf ./META-INF/ && rm -f ./VERSION
 
 javac -d ./classes/ -cp ".:./lib/jsoup-1.17.2.jar:./lib/org.json-chargebee-1.0.jar" ./src/*.java
 
